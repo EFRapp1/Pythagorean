9a81d1ea6c257b2d2ba6b0d47e08aaab87d94e1c
Ed <edrapp123@gmail.com>
2016-10-27 07:58:33 -0500

    standardized variable names

--- Triple.java
+++ Triple.java
@@ -2,45 +2,45 @@ package com.example;
 
 public class Triple {
 	
-static int Sides = 100;
-static int Count = 0;
+static int sides = 100;
+static int count = 0;
 
 //The main program will trap the start time, find the triples, and format the output
 public static void main(String[] args) {
 	long startTime = System.currentTimeMillis();
-	String Result = Finder(Sides);
+	String Result = Finder(sides);
 	int returnCode = OutputFormatter(Result, startTime);
 }
 
 public static String Finder(int length)
 {
 	// declare method variables.  we will need integer variables to hold values of each side
-	int SideA;
-	int SideB;
-	double SideC;
-	int Csquared;
+	int sideA;
+	int sideB;
+	double sideC;
+	int cSquared;
 	int i=1;
 	int ii=1;
-	String Buffer = "";
-	int Counter=0;
+	String buffer = "";
+	int counter=0;
 
 	while(i <= length)
 	{
-		SideA = i;
+		sideA = i;
 		while(ii <= i) // note the condition in the while loop removes duplicate combinations
 		{
-			SideB = ii;
-			Csquared = (SideA*SideA) + (SideB*SideB);  // determine value of C squared
-			SideC = java.lang.Math.sqrt(Csquared);	// find the square root
-			if (SideC == Math.floor(SideC) )	//Checking if C is an integer!
+			sideB = ii;
+			cSquared = (sideA*sideA) + (sideB*sideB);  // determine value of C squared
+			sideC = java.lang.Math.sqrt(cSquared);	// find the square root
+			if (sideC == Math.floor(sideC) )	//Checking if C is an integer!
 			{	
-				Counter++;
-				Buffer = Buffer + SideA;
-				Buffer = Buffer + ", ";
-				Buffer = Buffer + SideB;
-				Buffer = Buffer + ", ";
-				Buffer = Buffer + java.lang.Math.round(SideC);
-				Buffer = Buffer + " \n";		
+				counter++;
+				buffer = buffer + sideA;
+				buffer = buffer + ", ";
+				buffer = buffer + sideB;
+				buffer = buffer + ", ";
+				buffer = buffer + java.lang.Math.round(sideC);
+				buffer = buffer + " \n";		
 			}
 			ii=ii+1;	// increment for the loop used for SideB
 	}
@@ -48,21 +48,21 @@ public static String Finder(int length)
 	i=i+1;	//increment for while loop used for SideA
 }
 
-Count = Counter; // update the Count variable so it can be used in the output
-return Buffer; // return the results
+count = counter; // update the Count variable so it can be used in the output
+return buffer; // return the results
 }
 
 
-public static int OutputFormatter(String Output, long time){
+public static int OutputFormatter(String output, long time){
 	
-	String Intro ="";
-	Intro = Intro + "There are ";
-	Intro = Intro + Count;
-	Intro = Intro + " Pythagorean Triples with side lengths up to ";
-	Intro = Intro + Sides;
-	Intro = Intro + " units in length" + "\n";
-	String Screenoutput = Intro + Output;
-	System.out.println(Screenoutput);
+	String intro ="";
+	intro = intro + "There are ";
+	intro = intro + count;
+	intro = intro + " Pythagorean Triples with side lengths up to ";
+	intro = intro + sides;
+	intro = intro + " units in length" + "\n";
+	String screenOutput = intro + output;
+	System.out.println(screenOutput);
 	System.out.println("Total Run Time in milliseconds = ");
 	long finishTime = System.currentTimeMillis();
 	long elapsedTime = finishTime - time;
